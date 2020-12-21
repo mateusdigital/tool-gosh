6ecbaf1041570eadaff7036aa97482162733d700
stdmatt <stdmatt@pixelwizards.io>
2020-12-21 21:13:48 +0200

    Add some methods about paths.
    - PathSeparatorString
    - PathAlternateSeparatorString
    - PathSeparatorChar
    - PathAlternateSeparatorChar
    - PathCurrentDirectoryString
    - PathParentDirectoryString
    - IsCharAPathSeperator

--- src/OS.hpp
+++ src/OS.hpp
@@ -6,8 +6,25 @@
 
 namespace OS {
 
-String NewLineString() { return "\n"; }
+// @todo(stdmatt): Make this constants and inline... Dec 21, 2020
+inline String NewLineString      () { return "\n"; }
 
 
+// @todo(stdmatt): Make it for GNU and macOS - Dec 20, 2020
+inline String PathSeparatorString         () { return "/"; }
+inline String PathAlternateSeparatorString() { return "/"; }
+
+inline char   PathSeparatorChar           () { return '/'; }
+inline char   PathAlternateSeparatorChar  () { return '/'; }
+
+inline String PathCurrentDirectoryString() { return ".";  }
+inline String PathParentDirectoryString () { return ".."; }
+
+inline bool 
+IsCharAPathSeperator(char const c) 
+{
+    return PathSeparatorChar         () == c
+        || PathAlternateSeparatorChar() == c;
+}
 
 }; // namespace OS
