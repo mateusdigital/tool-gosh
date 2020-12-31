6bea847a9fad18f26c5a50b17b7438035ad0471e
stdmatt <stdmatt@pixelwizards.io>
2020-12-31 13:22:49 +0200

    Start to implement the CommandLine utils

---
+++ src/CommandLine.cpp
@@ -0,0 +1,242 @@
+// Header
+#include "CommandLine.hpp"
+
+namespace eaz {
+
+namespace Private {
+    Array<String> s_CommandLineComponents;
+} // namespace Private
+
+//
+//
+//
+void
+CommandLine::Set(i32 const argc, char const *argv[])
+{
+    Private::s_CommandLineComponents.Clear();
+    for(i32 i = 0; i < argc; ++i) {
+        Private::s_CommandLineComponents.PushBack(argv[i]);
+    }
+}
+
+void
+CommandLine::Set(String const &str)
+{
+    Private::s_CommandLineComponents = str.Split(' ');
+}
+    
+String
+CommandLine::GetAsString()
+{
+    return String::Join(Private::s_CommandLineComponents);
+}
+
+Array<String>
+CommandLine::GetAsArray()
+{
+    return Private::s_CommandLineComponents;
+}
+
+
+u32
+CommandLine::ArgsCount()
+{
+    return Private::s_CommandLineComponents.Count();
+}
+
+const char * const
+CommandLine::GetArgRaw(u32 const index)
+{
+    return Private::s_CommandLineComponents[index].CStr();
+}
+
+//
+//
+//
+CommandLine::Argument::Argument(
+    String                  const &short_name,
+    String                  const &long_name,
+    String                  const &description,
+    ArgumentParseCallback_t const &parse_callback)
+    : _short_name(short_name)
+    , _long_name(long_name)
+    , _description(description)
+    , _parse_callback(parse_callback)
+{
+    // @todo(stdmatt): Assert that things are not empty... Dec 30, 2020
+}
+
+//
+//
+//
+
+String const CommandLine::Parser::ShortFlagPrefix   = "-";
+String const CommandLine::Parser::LongFlagPrefix    = "--";
+String const CommandLine::Parser::WindowsFlagPrefix = "/";
+
+char const CommandLine::Parser::LongFlagSeparator    = '=';
+char const CommandLine::Parser::WindowsFlagSeparator = ':';
+
+
+CommandLine::Parser::Parser()
+    : _cmd_line(GetAsArray())
+{
+    // Empty...
+}
+
+CommandLine::Parser::Parser(String const &cmd_line_str)
+    : _cmd_line(cmd_line_str.Split(' '))
+{
+   // Empty...
+}
+
+CommandLine::Parser::Parser(i32 argc, char const *argv[])
+{
+    _cmd_line.Reserve(argc);
+    for(i32 i = 0; i < argc; ++i) {
+        _cmd_line.PushBack(argv[i]);
+    }
+}
+CommandLine::Parser::Parser(Array<String> const & cmd_line_items)
+    : _cmd_line(_cmd_line)
+{
+    // Empty...
+}
+
+CommandLine::Argument &
+CommandLine::Parser::CreateArgument(
+    String                            const &short_name,
+    String                            const &long_name,
+    String                            const &description,
+    Argument::ArgumentParseCallback_t const &parse_callback)
+{
+    // Try to find it before...
+    Argument *arg = FindArgumentByName(short_name);
+    if(arg) {
+        return *arg;
+    }
+    arg = FindArgumentByName(long_name);
+    if(arg) {
+        return *arg;
+    }
+
+    // Ok we don't have it, so let's create.
+    _arguments.EmplaceBack(short_name, long_name, description, parse_callback);
+    return _arguments.Back();
+}
+
+CommandLine::Argument const *
+CommandLine::Parser::FindArgumentByName(String const &name) const
+{
+    for(Argument const &arg : _arguments) {
+        if(arg.GetShortName() == name || arg.GetLongName() == name) {
+            return &arg;
+        }
+    }
+    return nullptr;
+}
+
+CommandLine::Parser::ParseResult_t
+CommandLine::Parser::Evaluate()
+{
+    Array<String> clean_components;
+    clean_components.Reserve(_cmd_line.Count());
+    // Clean up the components of the command line.
+    //   This will make ALL the items to be in a different string.
+    //   For example:
+    //      prog -abc          -> prog -a -b -c
+    //      prog --flag= value -> prog --flag value
+    for(
+        size_t i = 0,
+        cmd_line_count = _cmd_line.Count();
+        i < cmd_line_count;
+        ++i)
+    {
+        String const &item = _cmd_line[i];
+        if(IsShortFlag(item)) {
+            Array<String> split_flags = SplitShortFlag(item);
+            clean_components.PushBack(split_flags);
+        }
+        else if(IsLongFlag(item)) {
+            Array<String> split_flags = SplitLongFlag(item);
+            clean_components.PushBack(split_flags);
+        }
+        else {
+            clean_components.PushBack(item);
+        }
+    }
+    return ParseResult_t::Success(true);
+}
+
+bool
+CommandLine::Parser::IsShortFlag(String const &item) const
+{
+    if(item.IsEmptyOrWhitespace()) {
+        return false;
+    }
+    size_t const len = item.Length();
+    if(len < 2) {
+        return false;
+    }
+
+    if(item.StartsWith(Parser::LongFlagPrefix) || item.StartsWith(Parser::WindowsFlagPrefix)) {
+        return false;
+    }
+
+    return item.StartsWith(Parser::ShortFlagPrefix);
+}
+
+
+bool
+CommandLine::Parser::IsLongFlag(String const &item) const
+{
+   if(IsShortFlag(item)) {
+       return false;
+   }
+
+    if(item.StartsWith(Parser::LongFlagPrefix) || item.StartsWith(Parser::WindowsFlagPrefix)) {
+        return true;
+    }
+
+    return false;
+}
+
+Array<String>
+CommandLine::Parser::SplitShortFlag(String const &item) const
+{
+    // Given the arguments:
+    //    prog -abc
+    // We can split the flag in two different ways.
+    //    1) prog -a -b -c  : Each char becomes a flag.
+    //    2) prog -a bc     : -a is a flag and bc is the value of it.
+    // To decide we need to check the current Arguments and see if
+    // we have an argument with that name, if so it's a flag
+    // otherwise it's a value.
+    Array<String> split_items;
+    for(size_t i = 1, item_len = item.Length(); i < item_len; ++i) {
+        // @todo(stdmatt): O(n^2) just to find the flags, improve it - Dec 31, 2020.
+        // @todo(stdmatt): Creating a string just to pass a char - Dec 31, 2020.
+        char const c = item[i];
+        Argument const * const arg = FindArgumentByName(String(c));
+        if(arg) { // This is a flag...
+            split_items.PushBack(String::Format("-%c", c));
+        } else {
+            split_items.PushBack(String(c));
+        }
+    }
+
+    return split_items;
+}
+
+Array<String>
+CommandLine::Parser::SplitLongFlag(String const &item) const
+{
+    Array<String> split_items =  item.Split(
+        Array<char>{ Parser::LongFlagSeparator, Parser::WindowsFlagSeparator }
+    );
+
+    return split_items;
+}
+
+
+} // namespace eaz
---
+++ src/CommandLine.hpp
@@ -0,0 +1,148 @@
+#pragma once
+//
+#include <functional>
+// 
+#include "BasicTypes.hpp"
+#include "CodeUtils.hpp"
+#include "Macros.hpp"
+#include "Array.hpp"
+#include "String.hpp"
+#include "Result.hpp"
+
+
+namespace eaz { namespace CommandLine {
+
+//
+//
+//
+void Set(i32 const argc, char const *argv[]);
+void Set(String const &str);
+
+String        GetAsString();
+Array<String> GetAsArray ();
+
+u32                ArgsCount();
+const char * const GetArgRaw(u32 const index);
+
+
+//
+//
+//
+enum class ArgumentType
+{
+    Posix,    ///< [Flag] Single | [Prefix] '-'  | [Value] ' '      |  cmd   -f     value
+    GNU,      ///< [Flag] Multi  | [Prefix] '--' | [Value] ' ', '=' |  cmd --flag = value
+    Windows   ///< [Flag] Multi  | [Prefix] '/'  | [Value] ' ', ":' |  cmd  /flag : value
+}; // enum Argument Type
+
+
+class Argument
+{
+public:
+    enum class ParseStatus {
+        Valid, Invalid
+    }; // enum ParseStatus
+
+    typedef std::function<ParseStatus(String const &)> ArgumentParseCallback_t;
+
+public:
+    Argument( String                  const &short_name,
+        String                  const &long_name,
+        String                  const &description,
+        ArgumentParseCallback_t const &parse_callback);
+
+public:
+    String const & GetShortName  () const { return _short_name;  }
+    String const & GetLongName   () const { return _long_name;   }
+    String const & GetDescription() const { return _description; }
+
+    Array<String> const & GetValues  () const { return _values;            }
+    bool                  HasValues  () const { return !_values.IsEmpty(); }
+    size_t                ValuesCount() const { return !_values.Count  (); }
+
+public:
+    ParseStatus
+    ParseArgument(String const &value)
+    {
+        ParseStatus status = ParseStatus::Valid;
+
+        if(_parse_callback) {
+            status = _parse_callback(value);
+        }
+        if(status == ParseStatus::Valid) {
+            _values.PushBack(value);
+        }
+
+        return status;
+    }
+
+private:
+    String _short_name;
+    String _long_name;
+    String _description;
+
+    Array<String>           _values;
+    ArgumentParseCallback_t _parse_callback;
+}; // class Argument
+
+
+class Parser
+{
+public:
+    typedef Result<bool> ParseResult_t;
+
+    static String const ShortFlagPrefix;
+    static String const LongFlagPrefix;
+    static String const WindowsFlagPrefix;
+
+    static char const LongFlagSeparator;
+    static char const WindowsFlagSeparator;
+
+public:
+    explicit Parser();
+    explicit Parser(String const &cmd_line_str);
+    explicit Parser(i32 argc, char const *argv[]);
+    explicit Parser(Array<String> const & cmd_line_items);
+
+public:
+    Argument& CreateArgument(
+        String                            const &short_name,
+        String                            const &long_name,
+        String                            const &description,
+        Argument::ArgumentParseCallback_t const &parse_callback);
+
+    Argument const * FindArgumentByName(String const &name) const;
+    Argument       * FindArgumentByName(String const &name) {
+        Argument const * const arg = Cast<const Parser*>(this)->FindArgumentByName(name);
+        return Cast<Argument *>(arg);
+    }
+
+    Array<Argument> const & GetArguments       () const { return _arguments;         }
+    Array<String>   const & GetPositionalValues() const { return _positional_values; }
+
+public:
+    ParseResult_t Evaluate();
+
+private:
+    bool IsShortFlag(String const &item) const;
+    bool IsLongFlag(String const &item) const;
+
+    Array<String> SplitShortFlag(String const &item) const;
+    Array<String> SplitLongFlag (String const &item) const;
+
+
+private:
+    Array<Argument> _arguments;
+    Array<String>   _positional_values;
+    Array<String>   _cmd_line;
+}; // class Parser
+
+
+#define EAZ_ARG_FOUND_FUNC(_code_)                        \
+    [](String const &value) {                             \
+        _code_;                                           \
+        return CommandLine::Argument::ParseStatus::Valid; \
+    }
+
+} // namespace CommandLine
+} // namespace eaz
