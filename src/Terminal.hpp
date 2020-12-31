38a5db0a470e62a083bb50085fa34c79d62b4e87
stdmatt <stdmatt@pixelwizards.io>
2020-12-31 13:22:24 +0200

    Start to implement the Terminal Utilities

--- src/Terminal.hpp
+++ src/Terminal.hpp
@@ -1,9 +1,30 @@
+#pragma once
+
 //
+#include "BasicTypes.hpp"
 #include "String.hpp"
 
 
 namespace Terminal {
 
+//
+//
+//
+enum class OutputMode {
+    Always, 
+    Never, 
+    OnlyIfTerminal
+}; // enum OutputMode
+
+void       SetOutputMode(OutputMode const mode);
+OutputMode GetOutputMode(); 
+
+bool IsConnectedToTTY();
+
+
+//
+//
+//
 struct Color
 {
     static Color const None;
@@ -18,19 +39,34 @@ struct Color
     static Color const Cyan;
     static Color const Magenta;
     static Color const Yellow;
+    
+    constexpr explicit Color(u32 const v)
+        : value(v)
+    {
+        // Empty...
+    }
 
-};
+    u32 value;
+}; // struct Color
 
 struct Attributes
 {
-    static Attributes const & Empty;
-};
+    static Attributes const Empty;
 
+    constexpr explicit Attributes(u32 const v)
+        : value(v)
+    {
+        // Empty...
+    }
+
+    u32 value;
+}; // struct Attributes
 
 String Colored(
     String     const &str,
     Color      const fg_color = Color::None,
     Color      const bg_color = Color::None,
     Attributes const attr     = Attributes::Empty);
+
 
 }; // namespace Terminal
