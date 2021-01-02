b94ae29562c4d236dd030112ca6b50c3c540c3cc
stdmatt <stdmatt.smma@gmail.com>
2021-01-02 17:37:29 +0200

    Put inside the namespace and create the Min function

--- src/Math.hpp
+++ src/Math.hpp
@@ -2,8 +2,9 @@
 
 // Arkadia
 #include "Macros.hpp"
-namespace ark {
+namespace ark { namespace Math {
 
+// @todo(stdmatt): If is numeric is best to not pass by reference... Jan 02, 2021
 template<typename Type>
 ARK_FORCE_INLINE constexpr const Type&
 Clamp(Type const &_value, Type const &_min, Type const &_max)
@@ -13,4 +14,13 @@ Clamp(Type const &_value, Type const &_min, Type const &_max)
            _value;
 }
 
+// @todo(stdmatt): If is numeric is best to not pass by reference... Jan 02, 2021
+template<typename Type>
+ARK_FORCE_INLINE constexpr const Type&
+Min(Type const &a, Type const &b)
+{
+    return (a < b) ? a : b;
+}
+
+} // namespace Math
 } // namespace ark
