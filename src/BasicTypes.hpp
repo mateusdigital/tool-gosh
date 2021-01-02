843745c979dc475a86c8c7b6006fff89e887d5f6
stdmatt <stdmatt.smma@gmail.com>
2021-01-02 17:36:01 +0200

    Add the MinMax struct

--- src/BasicTypes.hpp
+++ src/BasicTypes.hpp
@@ -4,7 +4,12 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
+// Arkadia
+#include "Assert.hpp"
 
+//
+// Numeric Types
+//
 // uint
 typedef uint8_t   u8;
 typedef uint16_t u16;
@@ -18,3 +23,26 @@ typedef int64_t i64;
 // float
 typedef float  f32;
 typedef double f64;
+
+
+//
+//
+//
+namespace ark {
+
+template <typename T>
+struct MinMax
+{
+    T min;
+    T max;
+
+    MinMax(T min, T max)
+        : min(min)
+        , max(max)
+    {
+        // @todo(stdmatt): Format string is assuming ints... Jan 02, 2021
+        ARK_ASSERT(min <= max, "min needs to <= max - min(%d), max(%d)", min, max);
+    }
+}; // struct MinMax
+
+} // namespace ark
