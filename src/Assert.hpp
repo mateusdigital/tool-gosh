2916b7d9d4627c03b519b891c3129ffdf6eb93fc
stdmatt <stdmatt.smma@gmail.com>
2021-01-04 09:57:36 +0200

    Add the ARK_ASSERT_TRUE

--- src/Assert.hpp
+++ src/Assert.hpp
@@ -1,3 +1,5 @@
 #pragma once
 
+// @todo(stdmatt): Implement the assetions - Jan 02, 2021
 #define ARK_ASSERT(_expr_, _fmt_, ...) do { } while(0)
+#define ARK_ASSERT_TRUE(_expr_, _fmt_, ...) do { } while(0)
