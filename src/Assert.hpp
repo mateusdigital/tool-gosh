e6f862efdf660153ec97be8c63a06ee5027a978d
stdmatt <stdmatt.smma@gmail.com>
2021-01-08 10:09:44 +0200

    Put the Asserts on ifdefs to make it only enabled when ARK_ASSERTION_IS_ENABLED is set to true.

--- src/Assert.hpp
+++ src/Assert.hpp
@@ -1,5 +1,14 @@
 #pragma once
+// Arkadia
+#include "Discovery.hpp"
 
-// @todo(stdmatt): Implement the assetions - Jan 02, 2021
-#define ARK_ASSERT(_expr_, _fmt_, ...) do { } while(0)
-#define ARK_ASSERT_TRUE(_expr_, _fmt_, ...) do { } while(0)
+#if (ARK_ASSERTION_IS_ENABLED)
+    // @todo(stdmatt): Actually implement the assetions... Jan 06, 2021
+    #define ARK_ASSERT(_expr_, _fmt_, ...)       do { } while(0)
+    #define ARK_ASSERT_TRUE(_expr_, _fmt_, ...)  do { } while(0)
+    #define ARK_ASSERT_FALSE(_expr_, _fmt_, ...) do { } while(0)
+#else // (ARK_ASSERTION_IS_ENABLED)
+    #define ARK_ASSERT(_expr_, _fmt_, ...)       do { } while(0)
+    #define ARK_ASSERT_TRUE(_expr_, _fmt_, ...)  do { } while(0)
+    #define ARK_ASSERT_FALSE(_expr_, _fmt_, ...) do { } while(0)
+#endif // ARK_ENABLE_ASSERTIONS)
