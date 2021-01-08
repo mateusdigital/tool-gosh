ee7652a6299e1d83287238a7030b17a02d8437dd
stdmatt <stdmatt.smma@gmail.com>
2021-01-08 10:10:29 +0200

    Make the things based on the current compiler.

--- src/Macros.hpp
+++ src/Macros.hpp
@@ -1,18 +1,32 @@
 #pragma once
+// Arkadia
+#include "Discovery.hpp"
 
+//
+// Force Inline
+//
 
-#if (ARK_COMPILER == ARK_COMPILER_CLANG)
+#define ARK_INLINE   inline
+
+#if (ARK_COMPILER_IS_CLANG || ARK_COMPILER_IS_GCC)
     #define ARK_FORCE_INLINE    inline
     #define ARK_FORCE_NO_INLINE
-
-    #define ARK_OPTIMIZE_OFF _Pragma("clang optimize off")
-    #define ARK_OPTIMIZE_ON  _Pragma("clang optimize on")
-#else
+#elif (ARK_COMPILER_IS_MSVC)
     #define ARK_FORCE_INLINE    __forceinline
     #define ARK_FORCE_NO_INLINE _declspec(noinline)
+#else
+    #error "Compiler not supported...";
+#endif
 
-
+//
+// Optimizations
+//
+#if (ARK_COMPILER_IS_CLANG || ARK_COMPILER_IS_GCC)
+    #define ARK_OPTIMIZE_OFF _Pragma("clang optimize off")
+    #define ARK_OPTIMIZE_ON  _Pragma("clang optimize on")
+#elif (ARK_COMPILER_IS_MSVC)
     #define ARK_OPTIMIZE_OFF __pragma(optimize("", off))
     #define ARK_OPTIMIZE_ON  __pragma(optimize("", on ))
-
-#endif // if (ARK_COMPILER == ARK_COMPILER_CLANG)
+#else
+    #error "Compiler not supported...";
+#endif
